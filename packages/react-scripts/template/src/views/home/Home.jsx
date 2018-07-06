e547de25c56a7969c223be5a0315ce4a78abae02
ChrisPlease <chris@chrisplease.me>
2018-07-06 11:08:37 -0600

    reformat for bloom scaffold

--- packages/react-scripts/template/src/views/home/Home.jsx
+++ packages/react-scripts/template/src/views/home/Home.jsx
@@ -1,22 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
 
-class App extends Component {
+import logo from '../../assets/img/logo.svg';
+
+export class Home extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-          FOOOOOOOOO
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(null, null)(Home)
